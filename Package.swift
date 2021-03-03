b5dbd059703ce2564f9e86926c833b88a82b4543
Geoff Raeder <geoff@truqcapp.com>
2021-03-03 11:26:46 -0500

    Removed generated comments

--- Package.swift
+++ Package.swift
@@ -6,14 +6,11 @@ import PackageDescription
 let package = Package(
     name: "BugsplatMac",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BugsplatMac",
             targets: ["BugsplatMac"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "BugsplatMac",
             url: "https://github.com/BugSplat-Git/BugSplatMac/releases/download/1.1/BugsplatMac.xcframework.zip",
