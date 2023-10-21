a1d07a23e9fd9f962b3becd7405ac7c1244e3bed
Geoff Raeder <geoff@alltrails.com>
2023-10-21 14:14:56 -0700

    Updates Swift package config

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BugsplatMac",
-            url: "https://github.com/BugSplat-Git/BugSplat-macOS/releases/download/1.1.4/BugsplatMac.xcframework.zip",
-            checksum: "de54a05b58a3275d62f0cf8cae0654823b8c7165ab45712f41449a996b70ad2a"),
+            url: "https://github.com/BugSplat-Git/BugSplat-macOS/releases/download/1.1.5/BugsplatMac.xcframework.zip",
+            checksum: "132c951b438a82f18aa5e02e7c249c8db5dc07cd130f4448df2a572781199cb8"),
     ]
 )
